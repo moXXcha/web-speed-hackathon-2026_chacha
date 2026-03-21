d4619e16584c8e5600ae15727102cc5ae6585423
ota sho <chacha.love.sho@icloud.com>
2026-03-21 14:13:11 +0900

    全静的ファイルにキャッシュ設定、LCP preload削除

--- application/server/src/routes/static.ts
+++ application/server/src/routes/static.ts
@@ -1,11 +1,7 @@
-import { promises as fs } from "fs";
-import path from "path";
-
 import history from "connect-history-api-fallback";
 import { Router } from "express";
 import serveStatic from "serve-static";
 
-import { Post } from "@web-speed-hackathon-2026/server/src/models";
 import {
   CLIENT_DIST_PATH,
   PUBLIC_PATH,
@@ -13,33 +9,6 @@ import {
 } from "@web-speed-hackathon-2026/server/src/paths";
 
 export const staticRouter = Router();
-
-// ホーム画面のLCP改善: 最初の動画投稿のposter画像をpreload
-let cachedIndexHtml: string | null = null;
-
-staticRouter.get("/", async (_req, res, next) => {
-  try {
-    if (!cachedIndexHtml) {
-      cachedIndexHtml = await fs.readFile(path.join(CLIENT_DIST_PATH, "index.html"), "utf-8");
-    }
-
-    const firstMoviePost = await Post.findOne({
-      order: [["id", "DESC"]],
-    });
-
-    const movie = (firstMoviePost as any)?.movie;
-
-    if (movie) {
-      const preloadTag = `<link rel="preload" as="image" href="/movies/posters/${movie.id}.webp" fetchpriority="high">`;
-      const html = cachedIndexHtml.replace("</head>", `${preloadTag}\n</head>`);
-      res.type("html").send(html);
-    } else {
-      res.type("html").send(cachedIndexHtml);
-    }
-  } catch {
-    next();
-  }
-});
 
 // SPA 対応のため、ファイルが存在しないときに index.html を返す
 staticRouter.use(history());
@@ -49,9 +18,9 @@ staticRouter.use(
 );
 
 staticRouter.use(
-  serveStatic(PUBLIC_PATH),
+  serveStatic(PUBLIC_PATH, { maxAge: '1d' }),
 );
 
 staticRouter.use(
-  serveStatic(CLIENT_DIST_PATH),
+  serveStatic(CLIENT_DIST_PATH, { maxAge: '1d' }),
 );
